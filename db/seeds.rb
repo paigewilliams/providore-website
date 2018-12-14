083f9415dd95185a16032e65de41798878b74334
Paige Williams <paw145@humboldt.edu>
2018-12-14 09:56:06 -0800

    Add products page and seed product database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create!([
+  {"name": "Gruyere", "cost": "9.00", "country_of_origin": "France"},
+  {"name": "Piave Vecchio", "cost": "7.00", "country_of_origin": "Italy"},
+  {"name": "L'amuse Brabander", "cost": "13.00", "country_of_origin": "Holland"},
+  {"name": "Coastal Cheddar", "cost": "6.00", "country_of_origin": "England"}
+  ])
